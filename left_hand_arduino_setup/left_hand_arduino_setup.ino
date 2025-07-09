fb9a14abcd63e90f05de552fb7305a46233b2590
dasha <daria.ilina@tum.de>
2025-07-09 19:13:32 +0200

    changed how aduino sends data

--- left_hand_arduino_setup/left_hand_arduino_setup.ino
+++ left_hand_arduino_setup/left_hand_arduino_setup.ino
@@ -1,85 +1,67 @@
 #include <Arduino_BMI270_BMM150.h>
 #include <ArduinoBLE.h>
 
-BLEService imuService("12345678-1234-5678-1234-56789abc0000"); //service UUID
-
-//characteristic UUIDs
-// Accelerometer
-BLEFloatCharacteristic accX("00000001-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic accY("00000002-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic accZ("00000003-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-
-// Gyroscope
-BLEFloatCharacteristic gyroX("00000004-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic gyroY("00000005-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic gyroZ("00000006-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-
-// Magnetometer
-BLEFloatCharacteristic magX("00000007-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic magY("00000008-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
-BLEFloatCharacteristic magZ("00000009-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
+BLEService imuService("12345678-1234-5678-1234-56789abcdef0");  // custom
+BLECharacteristic imuChar("abcdef01-1234-5678-1234-56789abcdef0",
+                          BLERead | BLENotify, 36);  // 9 floats * 4 bytes, all values are sent as one
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
+  Serial.begin(115200); // initialize USB for debugging
+  while (!Serial);
 
-  //keep trying until IMU initializes
-  while (!IMU.begin()) {
-    Serial.println("IMU not found. Retrying...");
-    delay(1000);
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
   }
-  Serial.println("IMU initialized.");
 
-  //keep trying until BLE initializes
-  while (!BLE.begin()) {
-    Serial.println("BLE init failed. Retrying...");
-    delay(1000);
+  if (!BLE.begin()) {
+    Serial.println("Failed to initialize BLE!");
+    while (1);
   }
-  Serial.println("BLE initialized.");
-  Serial.println("Arduino is alive");
 
-  BLE.setLocalName("IMU_LeftHand"); //different for each arduino
+  BLE.setLocalName("IMU_LeftArm");
   BLE.setAdvertisedService(imuService);
 
-  imuService.addCharacteristic(accX);
-  imuService.addCharacteristic(accY);
-  imuService.addCharacteristic(accZ);
-  imuService.addCharacteristic(gyroX);
-  imuService.addCharacteristic(gyroY);
-  imuService.addCharacteristic(gyroZ);
-  imuService.addCharacteristic(magX);
-  imuService.addCharacteristic(magY);
-  imuService.addCharacteristic(magZ);
-
+  imuService.addCharacteristic(imuChar);
   BLE.addService(imuService);
   BLE.advertise();
+
+  Serial.println("Arduino is ready");
 }
 
 void loop() {
   BLEDevice central = BLE.central(); //device that arduino connects to
 
-  if (central && central.connected()) //if device has connected and is still connected
-  {
+  if (central) {
     while (central.connected()) {
-      float ax, ay, az, gx, gy, gz, mx, my, mz;
+      float accX, accY, accZ;
+      float gyroX, gyroY, gyroZ;
+      float magX, magY, magZ;
 
-      if (IMU.accelerationAvailable()) IMU.readAcceleration(ax, ay, az);
-      if (IMU.gyroscopeAvailable()) IMU.readGyroscope(gx, gy, gz);
-      if (IMU.magneticFieldAvailable()) IMU.readMagneticField(mx, my, mz);
+      if (IMU.accelerationAvailable()) {
+        IMU.readAcceleration(accX, accY, accZ);
+      }
 
-      accX.writeValue(ax);
-      accY.writeValue(ay);
-      accZ.writeValue(az);
+      if (IMU.gyroscopeAvailable()) {
+        IMU.readGyroscope(gyroX, gyroY, gyroZ);
+      }
 
-      gyroX.writeValue(gx);
-      gyroY.writeValue(gy);
-      gyroZ.writeValue(gz);
+      if (IMU.magneticFieldAvailable()) {
+        IMU.readMagneticField(magX, magY, magZ);
+      }
 
-      magX.writeValue(mx);
-      magY.writeValue(my);
-      magZ.writeValue(mz);
+      float values[9] = {
+        accX, accY, accZ,
+        gyroX, gyroY, gyroZ,
+        magX, magY, magZ
+      };
 
-      delay(100);  // Send data every 100 ms
+      // this automatically sends a notification if subscribed
+      imuChar.setValue((uint8_t*)values, sizeof(values)); //casts array to bytes
+
+      delay(10);  // 100 Hz
     }
+
+    Serial.println("Disconnected");
   }
 }
